4e249f1de57990391b94b7093724de4abb163193
renanpvaz <renan.piresvz@gmail.com>
2019-04-28 19:34:41 -0300

    Initial commit

---
+++ .storybook/config.js
@@ -0,0 +1,20 @@
+import { configure, addDecorator, addParameters } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { withOmission } from '../'
+
+addDecorator(
+  withInfo({
+    header: false,
+    inline: true,
+  }),
+)
+
+addParameters(withOmission())
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../stories', true, /\.stories\.js$/)
+function loadStories() {
+  req.keys().forEach(filename => req(filename))
+}
+
+configure(loadStories, module)
---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Omitted = Object.assign(props => props._, {
+  displayName: '...',
+})
+
+export const withOmission = () => ({
+  info: {
+    propTablesExclude: [Omitted],
+  },
+})
+export const setDisplayName = name => (Omitted.displayName = name)
+
+const omit = omitted => <Omitted _={omitted} /> // addon-info ignores props that start with "_"
+
+export default omit
